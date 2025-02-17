f139a11a23821b092b010c9d8d87885384ab7f5b
tamiayuastuti <2223617.tami@smkn-2sbg.sch.id>
2025-02-17 10:23:56 +0700

    menambahkan komentar

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
+// Ini mendefinisikan perintah inspire untuk Artisan (CLI Laravel). Ketika perintah ini dijalankan, ia akan menjalankan kode di dalam callback    
     $this->comment(Inspiring::quote());
+    //Menampilkan kutipan inspiratif yang diambil dari kelas Inspiring menggunakan metode quote().
 })->purpose('Display an inspiring quote')->hourly();
+//->purpose('Display an inspiring quote'): Memberikan deskripsi pada perintah Artisan ini, yaitu "Display an inspiring quote" (Menampilkan kutipan inspiratif)
+//->hourly();: Menjadwalkan perintah inspire agar dijalankan setiap jam.
