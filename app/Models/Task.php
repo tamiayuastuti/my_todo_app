95894c6074065ed9c107c1fce9ecb0f01de7147f
tamiayuastuti <2223617.tami@smkn-2sbg.sch.id>
2025-02-17 09:21:06 +0700

    menambahkan komentar

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\TaskList;
 
-class Task extends Model
+class Task extends Model 
+//class Task extends Model → Task adalah model Eloquent yang digunakan untuk berinteraksi dengan database.
 {
-    protected $fillable = [
+    protected $fillable = [ 
+    //$fillable → Menentukan kolom yang boleh diisi secara massal (mass assignment).
+    //Memungkinkan input otomatis ke kolom ini tanpa create([]).    
         'name',
         'description',
         'is_completed',
@@ -16,19 +19,27 @@ class Task extends Model
     ];
 
     protected $guarded = [
+    //$guarded → Menentukan kolom yang tidak boleh diisi secara massal.
+    //id, created_at, dan updated_at tidak bisa dimodifikasi langsung.    
         'id',
         'created_at',
         'updated_at'
     ];
 
     const PRIORITIES = [
+    //PRIORITIES → Konstanta untuk tingkatan prioritas tugas.
+    //Untuk menentukan nilai prioritas yang diperbolehkan.
         'low',
         'medium',
         'high'
     ];
 
     public function getPriorityClassAttribute() {
+    //Accessor: getPriorityClassAttribute()
+    //Mengembalikan kelas CSS berdasarkan tingkat prioritas (low = success, medium = warning, high = danger)    
         return match($this->attributes['priority']) {
+        //$this->attributes['priority'] → Mengambil nilai kolom priority dari model Task.
+        //match → Struktur kontrol yang mirip dengan switch, tetapi lebih singkat dan langsung mengembalikan hasil.    
             'low' => 'success',
             'medium' => 'warning',
             'high' => 'danger',
@@ -37,6 +48,7 @@ class Task extends Model
     }
 
     public function list() {
+    //Menandakan bahwa Task milik satu TaskList (hubungan many-to-one).    
         return $this->belongsTo(TaskList::class, 'list_id');
     }    
 }
--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+//TaskList adalah model yang digunakan untuk menyimpan dan mengelola data daftar tugas.
 class TaskList extends Model
 {
     protected $fillable = ['name'];
+    //$fillable → Menentukan kolom yang boleh diisi secara massal
+    //Hanya kolom name yang bisa diisi langsung.
     protected $guarded = [
+    //$guarded → Menentukan kolom yang tidak boleh diisi secara massal.    
+    //Mencegah perubahan langsung pada id, created_at, dan updated_at.
         'id',
         'created_at',
         'updated_at'
@@ -15,5 +20,8 @@ class TaskList extends Model
 
     public function tasks() {
         return $this->hasMany(Task::class, 'list_id');
+        //Relasi: hasMany()
+        //TaskList memiliki banyak Task (one-to-many relationship).
+        //Menghubungkan TaskList dengan Task melalui list_id.
     }
 }
