3814ca5ed60b0e5b9a94966093cf307196702437
tamiayuastuti <2223617.tami@smkn-2sbg.sch.id>
2025-02-17 09:43:38 +0700

    menambahkan komentar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,15 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
+//AppServiceProvider adalah service provider dalam Laravel yang digunakan untuk mendaftarkan layanan dan mengatur konfigurasi aplikasi.
+//Service Provider → Mengelola layanan yang digunakan dalam aplikasi.
 {
     /**
      * Register any application services.
      */
     public function register(): void
+    //register() → Mendaftarkan layanan ke dalam container Laravel.
+    //Digunakan untuk mendaftarkan service yang tidak langsung digunakan saat aplikasi dijalankan.
     {
         //
     }
@@ -18,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
+    //boot() → Menjalankan konfigurasi atau logika awal saat aplikasi berjalan.
+    //
     {
         //
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,28 @@
 <?php
 
 use Illuminate\Foundation\Application;
+//Mengimpor kelas utama Laravel yang digunakan untuk membuat dan mengatur aplikasi.
 use Illuminate\Foundation\Configuration\Exceptions;
+//Mengimpor konfigurasi untuk menangani error dan exception dalam aplikasi
 use Illuminate\Foundation\Configuration\Middleware;
+//Mengimpor konfigurasi untuk mengelola middleware, seperti autentikasi atau logging
 
 return Application::configure(basePath: dirname(__DIR__))
+//Application::configure() digunakan untuk mengatur direktori utama aplikasi.
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        //web → Menghubungkan aplikasi dengan file web routes (routes/web.php).
+        //commands → Menghubungkan dengan perintah konsol (routes/console.php).
+        //health → Endpoint /up untuk mengecek apakah aplikasi berjalan.
     )
     ->withMiddleware(function (Middleware $middleware) {
+    //Tempat untuk menambahkan middleware (misalnya autentikasi atau log request).    
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    //Tempat untuk mengatur bagaimana aplikasi menangani error dan exception.    
     })->create();
+    //Membuat instance aplikasi Laravel dan menjalankannya.
